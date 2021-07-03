bf014d5adcb367aa7b14b76d5af88cca9648ec3c
Ezio Lin <ninesuns.lin@outlook.com>
2021-07-03 17:51:48 +0800

    🎉 begin project!!

---
+++ src/main/java/cn/eziolin/zhiwei4idea/VuCodeCompletionContributor.java
@@ -0,0 +1,16 @@
+package cn.eziolin.zhiwei4idea;
+
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PlainTextTokenTypes;
+
+public class VuCodeCompletionContributor extends CompletionContributor {
+    public VuCodeCompletionContributor() {
+        extend(
+                CompletionType.BASIC,
+                PlatformPatterns.psiElement(PlainTextTokenTypes.PLAIN_TEXT),
+                new VuCodeCompletionProvider()
+        );
+    }
+}
