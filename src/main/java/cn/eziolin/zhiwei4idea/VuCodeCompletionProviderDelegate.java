0a0bdc220dd0115296619274df1b1b2c21794c1b
Ezio Lin <ninesuns.lin@outlook.com>
2021-07-15 22:42:38 +0800

    🚸 update: better prefix mather for completions
    补充 prefixMatcher 使得在输入过程中补全填充更加自然

--- src/main/java/cn/eziolin/zhiwei4idea/VuCodeCompletionProviderDelegate.java
+++ src/main/java/cn/eziolin/zhiwei4idea/VuCodeCompletionProviderDelegate.java
@@ -3,33 +3,29 @@ package cn.eziolin.zhiwei4idea;
 import cn.eziolin.zhiwei4idea.api.ZhiweiApi;
 import cn.eziolin.zhiwei4idea.api.model.BaseResponse;
 import cn.eziolin.zhiwei4idea.api.model.ViewMeta;
-import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.application.ex.ApplicationUtil;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.util.TextRange;
 
 import java.net.http.HttpResponse;
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
-import java.util.function.Function;
+import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class VuCodeCompletionProviderDelegate implements Function<CompletionParameters, Optional<List<LookupElementBuilder>>> {
-
+public class VuCodeCompletionProviderDelegate implements
+        BiConsumer<CompletionParameters, CompletionResultSet> {
     private static final String prefix = "card::";
     private static final Pattern pattern = Pattern.compile(prefix + "([\\S]+)$");
 
-
-    @Override
-    public Optional<List<LookupElementBuilder>> apply(CompletionParameters parameters) {
-        return getBeforeInputString(parameters).flatMap(this::getCards);
-    }
-
-    private Optional<String> getBeforeInputString(CompletionParameters parameters) {
+    public static Optional<String> getBeforeInputString(CompletionParameters parameters) {
         var position = parameters.getPosition();
         int offsetInFile = parameters.getOffset();
         var startAndEnd = position.getText().substring(0, offsetInFile);
@@ -40,14 +36,13 @@ public class VuCodeCompletionProviderDelegate implements Function<CompletionPara
         return Optional.empty();
     }
 
-
-    private Optional<List<LookupElementBuilder>> getCards(String searchKeyword) {
-            return searchCards(searchKeyword)
-            .map(
-                (it) -> it.body().get()
-            ).map(
-                (it) -> it.resultValue.caches.stream()
-                        .map((card -> LookupElementBuilder
+    private static Optional<List<LookupElementBuilder>> getCards(String searchKeyword) {
+        return searchCards(searchKeyword)
+                .map(
+                        (it) -> it.body().get()
+                ).map(
+                        (it) -> it.resultValue.caches.stream()
+                                .map((card -> LookupElementBuilder
                                         .create("#" + card.code)
                                         .withTypeText(card.name)
                                         .withLookupString(card.name)
@@ -56,14 +51,15 @@ public class VuCodeCompletionProviderDelegate implements Function<CompletionPara
                                             var startOffset = context.getStartOffset();
                                             var tailOffset = context.getTailOffset();
                                             var lookupString = item.getLookupString();
-                                            var prefixIndex = document.getText(new TextRange(0, startOffset)).lastIndexOf(prefix);
+                                            var prefixIndex =
+                                                    document.getText(new TextRange(0, startOffset)).lastIndexOf(prefix);
                                             document.replaceString(prefixIndex, tailOffset, lookupString);
                                         }))
-                        )).collect(Collectors.toList())
-            );
+                                )).collect(Collectors.toList())
+                );
     }
 
-    private Optional<HttpResponse<Supplier<BaseResponse<ViewMeta>>>> searchCards(String keyword) {
+    private static Optional<HttpResponse<Supplier<BaseResponse<ViewMeta>>>> searchCards(String keyword) {
         return ZhiweiApi.searchCard(keyword).map(
                 (response) -> {
                     try {
@@ -72,8 +68,21 @@ public class VuCodeCompletionProviderDelegate implements Function<CompletionPara
                                 ProgressManager.getInstance().getProgressIndicator()
                         );
                     } catch (ExecutionException e) {
-                       return  null;
+                        return null;
                     }
+                }
+        );
+    }
+
+    @Override
+    public void accept(CompletionParameters parameters, CompletionResultSet resultSet) {
+        getBeforeInputString(parameters).map(
+                keyword -> new AbstractMap.SimpleEntry<>(resultSet.withPrefixMatcher(keyword), keyword)
+        ).ifPresent(
+                simpleEntry -> {
+                    var result = simpleEntry.getKey();
+                    var keyword = simpleEntry.getValue();
+                    getCards(keyword).ifPresent(result::addAllElements);
                 }
         );
     }
--- src/main/java/cn/eziolin/zhiwei4idea/idea/VuCodeCompletionProvider.java
+++ src/main/java/cn/eziolin/zhiwei4idea/idea/VuCodeCompletionProvider.java
@@ -4,21 +4,18 @@ import cn.eziolin.zhiwei4idea.idea.service.CompletionService;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.util.ProcessingContext;
 import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.BiConsumer;
 
 public class VuCodeCompletionProvider extends CompletionProvider<CompletionParameters> {
 
-    private final Function<CompletionParameters, Optional<List<LookupElementBuilder>>> addCompletionsFn;
+    private final BiConsumer<CompletionParameters, CompletionResultSet> addCompletionsFn;
 
     public VuCodeCompletionProvider(
-            Function<CompletionParameters, Optional<List<LookupElementBuilder>>> addCompletionsFn
+            BiConsumer<CompletionParameters, CompletionResultSet> addCompletionsFn
     ) {
         this.addCompletionsFn = addCompletionsFn;
     }
@@ -29,7 +26,7 @@ public class VuCodeCompletionProvider extends CompletionProvider<CompletionParam
                                   @NotNull CompletionResultSet result) {
         var project = parameters.getPosition().getProject();
         if (CollectionUtils.isNotEmpty(project.getService(CompletionService.class).getAffectedFiles())) {
-            addCompletionsFn.apply(parameters).ifPresent(result::addAllElements);
+            addCompletionsFn.accept(parameters, result);
         }
     }
 
