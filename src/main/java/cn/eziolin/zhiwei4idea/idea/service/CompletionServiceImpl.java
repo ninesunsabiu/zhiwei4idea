99fe15fb4a716190f02382e424fde2b18a225083
Ezio Lin <ninesuns.lin@outlook.com>
2021-11-28 09:40:38 +0800

    🐛 解决在非 git 项目时使用插件的 NPE 问题
    没有 git 控制的话，setCheckinProjectPanel 不会被调用
    导致 myPanelRef 为 null 调用时产生 NPE

--- src/main/java/cn/eziolin/zhiwei4idea/idea/service/CompletionServiceImpl.java
+++ src/main/java/cn/eziolin/zhiwei4idea/idea/service/CompletionServiceImpl.java
@@ -8,54 +8,60 @@ import com.intellij.ui.EditorTextComponent;
 
 import javax.swing.event.AncestorEvent;
 import java.io.File;
+import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 
 public class CompletionServiceImpl implements CompletionService, Disposable {
-  private final AncestorListenerAdapter whenCheckinPanelShownChange =
+  private final AncestorListenerAdapter myWhenCheckinPanelShownChange =
       new AncestorListenerAdapter() {
         @Override
         public void ancestorAdded(AncestorEvent event) {
           ZhiweiApi.initSdk();
         }
       };
-  private WeakReference<CheckinProjectPanel> panelRef;
+
+  private WeakReference<CheckinProjectPanel> myPanelRef = null;
+
+  private Optional<CheckinProjectPanel> getCheckinProjectPanel() {
+    return Optional.ofNullable(myPanelRef).map(Reference::get);
+  }
 
   @Override
   public void setCheckinProjectPanel(CheckinProjectPanel panel) {
-    panel.getPreferredFocusedComponent().addAncestorListener(whenCheckinPanelShownChange);
-    panelRef = new WeakReference<>(panel);
+    panel.getPreferredFocusedComponent().addAncestorListener(myWhenCheckinPanelShownChange);
+    myPanelRef = new WeakReference<>(panel);
   }
 
   @Override
   public Boolean isCommitUiActive() {
-    CheckinProjectPanel panel = panelRef.get();
-    if (null == panel) {
-      return false;
-    }
-    var component = panel.getPreferredFocusedComponent();
-    return component instanceof EditorTextComponent
-        && ((EditorTextComponent) component).getComponent().isFocusOwner();
+    return getCheckinProjectPanel()
+        .map(
+            it -> {
+              var component = it.getPreferredFocusedComponent();
+              return component instanceof EditorTextComponent
+                  && ((EditorTextComponent) component).getComponent().isFocusOwner();
+            })
+        .orElse(false);
   }
 
   @Override
   public Collection<File> getAffectedFiles() {
-    CheckinProjectPanel panel = panelRef.get();
-    if (panel != null && isCommitUiActive()) {
-      return panel.getFiles();
-    }
-    return Collections.emptyList();
+    return getCheckinProjectPanel()
+        .map(it -> isCommitUiActive() ? it.getFiles() : null)
+        .orElse(Collections.emptyList());
   }
 
   @Override
   public void dispose() {
-    Optional.ofNullable(panelRef.get())
+    getCheckinProjectPanel()
         .ifPresent(
             it ->
                 it.getPreferredFocusedComponent()
-                    .removeAncestorListener(whenCheckinPanelShownChange));
-    panelRef = null;
+                    .removeAncestorListener(myWhenCheckinPanelShownChange));
+
+    myPanelRef = null;
   }
 }
